ac9ff292e391fd1eb2ac2d28a532b801beec570b
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-08-22 17:27:05 +0530

    fix: added extendTheme api

--- example/storybook/.storybook/preview.js
+++ example/storybook/.storybook/preview.js
@@ -1,6 +1,6 @@
 import { addParameters } from '@storybook/client-api';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
-import { Center, GluestackUIProvider } from '@gluestack-ui/themed';
+import { Center, GluestackUIProvider, NativeBaseProvider, extendTheme} from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/themed';
 import { useState } from 'react';
 export const parameters = {
@@ -93,6 +93,63 @@ export const decorators = [
         return isDark ? 'dark' : 'light';
       }
     }
+
+    // console.log(NativeBaseProvider, "h")
+
+    return (
+      <NativeBaseProvider config={config.theme} theme={extendTheme({colors: {
+        // Add new color
+        primary: {
+          50: '#E3F2F9',
+          100: '#C5E4F3',
+          200: '#A2D4EC',
+          300: '#7AC1E4',
+          400: '#47A9DA',
+          500: '#0088CC',
+          600: '#007AB8',
+          700: '#006BA1',
+          800: '#005885',
+          900: '#003F5E',
+        },
+        // Redefining only one shade, rest of the color will remain same.
+        amber: {
+          400: '#d97706',
+        },
+      },
+      
+      
+      components: {
+        Box: {
+          baseStyle: {
+            color: 'emerald.400'
+          },
+          defaultProps: {
+            size: 'lg'
+          },
+          sizes: {
+            xl: {
+              fontSize: '64px'
+            },
+            lg: {
+              fontSize: '32px'
+            },
+            md: {
+              fontSize: '16px'
+            },
+            sm: {
+              fontSize: '12px'
+            }
+          }
+        }
+      }
+
+      })} colorMode={getColorMode()}>
+        <Center>
+          <Story />
+        </Center>
+      </NativeBaseProvider>
+    );
+
     return (
       <GluestackUIProvider config={config.theme} colorMode={getColorMode()}>
         <Center>
