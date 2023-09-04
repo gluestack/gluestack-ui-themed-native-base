4225fb2f68e2f3cbc495a95032f8ca1cb46010d3
Akash Gautam <akashgautam@geekyants.com>
2023-09-04 13:21:48 +0530

    feat: badge done

--- example/storybook/src/components/DataDisplay/Badge/Badge.tsx
+++ example/storybook/src/components/DataDisplay/Badge/Badge.tsx
@@ -1,77 +1,22 @@
 import React from 'react';
-import {
-  Center,
-  Badge,
-  BadgeText,
-  BadgeIcon,
-  Icon,
-  Box,
-  Text,
-  VStack,
-  HStack,
-  SettingsIcon,
-  Divider,
-  AddIcon,
-  Image,
-  CheckIcon,
-  Heading,
-  Avatar,
-  AvatarFallbackText,
-  AvatarImage,
-  Button,
-  ButtonText,
-  Menu,
-  MenuIcon,
-  MenuItem,
-  MenuItemLabel,
-} from '@gluestack-ui/themed';
-import {
-  PaintBucket,
-  PuzzleIcon,
-  GlobeIcon,
-  BadgeCheckIcon,
-  BadgePlusIcon,
-} from 'lucide-react-native';
+import { Badge, GlobeIcon } from '@gluestack-ui/themed';
 
 const BadgeStory = ({ text = 'New feature', ...props }: any) => {
   return (
-    <Badge {...props}>
-      <BadgeText>{text}</BadgeText>
-      <BadgeIcon ml="$1" as={GlobeIcon} />
+    <Badge
+      leftIcon={GlobeIcon}
+      endIcon={GlobeIcon}
+      {...props}
+      sx={{
+        _icon: {
+          h: 16,
+          w: 16,
+        },
+      }}
+    >
+      {text}
     </Badge>
   );
 };
 
 export default BadgeStory;
-
-export {
-  Center,
-  Badge,
-  BadgeText,
-  BadgeIcon,
-  Icon,
-  Box,
-  Text,
-  VStack,
-  HStack,
-  PuzzleIcon,
-  SettingsIcon,
-  AddIcon,
-  PaintBucket,
-  Divider,
-  Image,
-  CheckIcon,
-  Heading,
-  Avatar,
-  AvatarFallbackText,
-  AvatarImage,
-  Button,
-  ButtonText,
-  GlobeIcon,
-  BadgeCheckIcon,
-  Menu,
-  MenuIcon,
-  MenuItem,
-  MenuItemLabel,
-  BadgePlusIcon,
-};
