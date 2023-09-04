e22eff6276c337cb350b65484569d02c5cfcc6b4
Akash Gautam <akashgautam@geekyants.com>
2023-09-04 18:53:18 +0530

    feat: divider done

--- example/storybook/src/components/DataDisplay/Divider/Divider.tsx
+++ example/storybook/src/components/DataDisplay/Divider/Divider.tsx
@@ -1,41 +1,19 @@
 import React from 'react';
-import {
-  Text,
-  Divider,
-  VStack,
-  HStack,
-  Box,
-  Center,
-  Heading,
-  Button,
-  ButtonText,
-} from '@gluestack-ui/themed';
+import { Text, Divider, VStack, HStack } from '@gluestack-ui/themed';
 
 const DividerStory = () => {
   return (
-    <VStack space="md" sx={{ justifyContent: 'center', alignItems: 'center' }}>
-      <HStack
-        sx={{
-          h: 40,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+    <VStack space="md" justifyContent="center" alignItems="center">
+      <HStack h={40} justifyContent="center" alignItems="center">
         <Text>Simple</Text>
         <Divider orientation="vertical" sx={{ mx: '$3' }} />
         <Text>Easy</Text>
         <Divider sx={{ mx: '$3' }} orientation="vertical" />
         <Text>Beautiful</Text>
       </HStack>
-      <VStack
-        sx={{
-          w: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <VStack w="100%" justifyContent="center" alignItems="center">
         <Text>Firefox</Text>
-        <Divider orientation="horizontal" sx={{ my: '$2' }} />
+        <Divider thickness={10} orientation="horizontal" sx={{ my: '$2' }} />
         <Text>Chrome</Text>
       </VStack>
     </VStack>
@@ -43,15 +21,3 @@ const DividerStory = () => {
 };
 
 export default DividerStory;
-
-export {
-  Text,
-  VStack,
-  HStack,
-  Divider,
-  Box,
-  Center,
-  Heading,
-  Button,
-  ButtonText,
-};
