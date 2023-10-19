7446d76b669535bdbdd595639d09658f89a7f744
Akash Gautam <akashgautam@geekyants.com>
2023-10-19 12:08:20 +0530

    fix: alert example

--- example/storybook/src/components/Feedback/Alert/Alert.tsx
+++ example/storybook/src/components/Feedback/Alert/Alert.tsx
@@ -1,23 +1,11 @@
 //@ts-nocheck
 import React from 'react';
 import { Alert, Text } from '@gluestack-ui/themed';
-import {
-  AlertCircleIcon,
-  InfoIcon,
-  CheckCircle2Icon,
-  XCircleIcon,
-} from 'lucide-react-native';
 
 function AlertStory({ ...props }: any) {
-  const Icon = () => {
-    if (props.status === 'success') return CheckCircle2Icon;
-    if (props.status === 'error') return XCircleIcon;
-    if (props.status === 'warning') return AlertCircleIcon;
-    return InfoIcon;
-  };
   return (
     <Alert {...props}>
-      <Alert.Icon mr="0" as={Icon()} />
+      <Alert.Icon mr="0" />
       <Text>Selection successfully moved!</Text>
     </Alert>
   );
