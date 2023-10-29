130e7b60cd4bb6f7094396279ff72f4638048fce
Akash Gautam <akashgautam@geekyants.com>
2023-10-29 23:54:05 +0530

    fix: skeleton text

--- example/storybook/src/components/Feedback/Alert/Alert.tsx
+++ example/storybook/src/components/Feedback/Alert/Alert.tsx
@@ -1,13 +1,6 @@
 //@ts-nocheck
 import React from 'react';
-import {
-  Alert,
-  Text,
-  Center,
-  Button,
-  Slide,
-  useColorModeValue,
-} from '@gluestack-ui/themed';
+import { Alert, Text } from '@gluestack-ui/themed';
 
 function AlertStory({ ...props }: any) {
   return (
@@ -16,71 +9,8 @@ function AlertStory({ ...props }: any) {
         <Alert.Icon mr="0" />
         <Text>Selection successfully moved!</Text>
       </Alert>
-      <Example />
     </>
   );
 }
 
 export default AlertStory;
-// const Example = () => {
-//   const [isOpen, setIsOpen] = React.useState(false);
-//   return (
-//     <Center>
-//       <Button shadow={2} onPress={() => setIsOpen(!isOpen)}>
-//         {isOpen ? 'Hide' : 'Show'}
-//       </Button>
-//       <PresenceTransition
-//         visible={isOpen}
-//         initial={{
-//           opacity: 0,
-//         }}
-//         animate={{
-//           opacity: 1,
-//           transition: {
-//             duration: 250,
-//           },
-//         }}
-//       >
-//         <Center
-//           mt="7"
-//           bg="teal.500"
-//           rounded="md"
-//           w="200px"
-//           h="100px"
-//           _text={{
-//             color: 'white',
-//           }}
-//           shadow={2}
-//         >
-//           Fade
-//         </Center>
-//       </PresenceTransition>
-//     </Center>
-//   );
-// };
-const Example = () => {
-  const [isOpenTop, setIsOpenTop] = React.useState(false);
-  const str = `${isOpenTop ? 'Hide' : 'Check Internet Connection'}`;
-  return (
-    <Center h="32">
-      <Slide in={isOpenTop} placement="left">
-        <Alert justifyContent="center" status="error" safeAreaTop={8}>
-          <Alert.Icon />
-          <Text color="error.600" fontWeight="medium">
-            No Internet Connection
-          </Text>
-        </Alert>
-      </Slide>
-      <Button
-        onPress={() => setIsOpenTop(!isOpenTop)}
-        variant="unstyled"
-        bg="coolGray.700:alpha.30"
-        _text={{
-          color: useColorModeValue('darkText', 'lightText'),
-        }}
-      >
-        {str}
-      </Button>
-    </Center>
-  );
-};
--- example/storybook/src/components/Feedback/Skeleton/Skeleton.tsx
+++ example/storybook/src/components/Feedback/Skeleton/Skeleton.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {
-  Center,
-  VStack,
-  Skeleton,
-  HStack,
-  // Alert,
-  // Text,
-} from '@gluestack-ui/themed';
+import { Center, VStack, Skeleton, HStack } from '@gluestack-ui/themed';
 
-function AlertStory() {
+function SkeletonStory() {
   return (
     <Center w="100%">
       <VStack
@@ -30,22 +23,22 @@ function AlertStory() {
           borderWidth={1}
           borderColor="coolGray.200"
           endColor="warmGray.50"
-          size={20}
+          size="80"
           rounded="full"
           mt="-70"
         />
         <HStack space="2">
-          <Skeleton size={20} rounded="full" />
-          <Skeleton size={20} rounded="full" />
-          <Skeleton size={20} rounded="full" />
-          <Skeleton size={20} rounded="full" />
-          <Skeleton size={20} rounded="full" />
+          <Skeleton size="20" rounded="full" />
+          <Skeleton size="20" rounded="full" />
+          <Skeleton size="20" rounded="full" />
+          <Skeleton size="20" rounded="full" />
+          <Skeleton size="20" rounded="full" />
         </HStack>
-        <Skeleton.Text lines={6} alignItems="center" px="12" />
+        <Skeleton.Text lines={3} alignItems="center" px="12" />
         <Skeleton mb="3" w="40" rounded="20" />
       </VStack>
     </Center>
   );
 }
 
-export default AlertStory;
+export default SkeletonStory;
