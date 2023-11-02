725dc3221e4ca57ebc8e8d5c3083d01c7c2a17f0
Akash Gautam <akashgautam@geekyants.com>
2023-11-02 17:00:35 +0530

    fix: actionsheer story

--- example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
+++ example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
-import {
-  Actionsheet,
-  Box,
-  // ActionsheetBackdrop,
-  // ActionsheetContent,
-  // ActionsheetDragIndicator,
-  // ActionsheetDragIndicatorWrapper,
-  // ActionsheetItem,
-  // ActionsheetItemText,
-  // useDisclose,
-  Text,
-  Button,
-} from '@gluestack-ui/themed';
+import { Actionsheet, Box, Text, Button } from '@gluestack-ui/themed';
 
 function ActionsheetStory({
   showActionsheet: _showActionsheetProp = true,
 }: any) {
   const [showActionsheet, setShowActionsheet] = useState(false);
+  const [hideDragIndicator, setHideDragIndicator] = useState(true);
 
   const handleClose = () => setShowActionsheet(false);
-  // const { isOpen, onOpen, onClose } = useDisclose();
+
   return (
     <>
       <Button
@@ -29,7 +18,19 @@ function ActionsheetStory({
       >
         Open
       </Button>
-      <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
+      <Button
+        onPress={() => {
+          setHideDragIndicator(!hideDragIndicator);
+        }}
+      >
+        {`${hideDragIndicator ? 'show' : 'hide'} DragIndicator`}
+      </Button>
+      <Actionsheet
+        isOpen={showActionsheet}
+        onClose={handleClose}
+        hideDragIndicator={hideDragIndicator}
+        disableOverlay={false}
+      >
         <Actionsheet.Content>
           <Box w="100%" h={60} px={4} justifyContent="center">
             <Text
@@ -42,44 +43,17 @@ function ActionsheetStory({
               Albums
             </Text>
           </Box>
-          <Actionsheet.Item>Delete</Actionsheet.Item>
+          <Actionsheet.Item isLoading _text={{ color: 'red.500' }}>
+            Delete
+          </Actionsheet.Item>
           <Actionsheet.Item isDisabled>Share</Actionsheet.Item>
-          <Actionsheet.Item>Play</Actionsheet.Item>
+          <Actionsheet.Item isLoading>Play</Actionsheet.Item>
           <Actionsheet.Item>Favourite</Actionsheet.Item>
           <Actionsheet.Item>Cancel</Actionsheet.Item>
         </Actionsheet.Content>
       </Actionsheet>
     </>
   );
-  // return (
-  //   <Actionsheet
-  //     isOpen={showActionsheet || showActionsheetProp}
-  //     onClose={handleClose}
-  //     {...props}
-  //   >
-  //     <ActionsheetBackdrop />
-  //     <ActionsheetContent>
-  //       <ActionsheetDragIndicatorWrapper>
-  //         <ActionsheetDragIndicator />
-  //       </ActionsheetDragIndicatorWrapper>
-  //       <ActionsheetItem onPress={handleClose} isDisabled>
-  //         <ActionsheetItemText>Delete</ActionsheetItemText>
-  //       </ActionsheetItem>
-  //       <ActionsheetItem onPress={handleClose}>
-  //         <ActionsheetItemText>Share</ActionsheetItemText>
-  //       </ActionsheetItem>
-  //       <ActionsheetItem onPress={handleClose}>
-  //         <ActionsheetItemText>Play</ActionsheetItemText>
-  //       </ActionsheetItem>
-  //       <ActionsheetItem onPress={handleClose}>
-  //         <ActionsheetItemText>Favourite</ActionsheetItemText>
-  //       </ActionsheetItem>
-  //       <ActionsheetItem onPress={handleClose}>
-  //         <ActionsheetItemText>Cancel</ActionsheetItemText>
-  //       </ActionsheetItem>
-  //     </ActionsheetContent>
-  //   </Actionsheet>
-  // );
 }
 
 export default ActionsheetStory;
