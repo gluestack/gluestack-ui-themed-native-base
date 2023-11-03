ca37c9c9be81f289db14b178d84b8da30c197c5b
Akash Gautam <akashgautam@geekyants.com>
2023-11-03 17:16:41 +0530

    fix: hstack and vstack

--- example/storybook/src/components/Forms/Textarea/Textarea.tsx
+++ example/storybook/src/components/Forms/Textarea/Textarea.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Textarea } from '@gluestack-ui/themed';
+import { TextArea } from '@gluestack-ui/themed';
 
 const TextareaStory = ({ ...props }: any) => {
-  return <Textarea {...props} mx="$2" placeholder="Your text goes here..." />;
+  return <TextArea {...props} mx="$2" placeholder="Your text goes here..." />;
 };
 
 export default TextareaStory;
--- example/storybook/src/components/Layout/VStack/VStack.tsx
+++ example/storybook/src/components/Layout/VStack/VStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { VStack, Box } from '@gluestack-ui/themed';
+import { VStack, Box, Divider } from '@gluestack-ui/themed';
 
 const VStackStory = ({ space, reversed, ...props }: any) => {
   return (
@@ -10,6 +10,7 @@ const VStackStory = ({ space, reversed, ...props }: any) => {
       sx={{ justifyContent: 'center', alignItems: 'center' }}
       reversed={reversed}
       {...props}
+      divider={<Divider h="10" />}
     >
       <Box w={100} h={100} bg="success.500" />
       <Box w={100} h={100} bg="blue.400" />
