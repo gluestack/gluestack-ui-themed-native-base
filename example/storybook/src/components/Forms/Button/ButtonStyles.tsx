294c633a63e989c1a45b5c503f678fa1ca581b53
Vidhi Kataria <katariavidhi99@gmail.com>
2023-11-06 15:40:09 +0530

    fix: stories

--- example/storybook/src/components/Forms/Button/ButtonStyles.tsx
+++ example/storybook/src/components/Forms/Button/ButtonStyles.tsx
@@ -1,5 +1,5 @@
 import type { ComponentStory } from '@storybook/react-native';
-import { Button, ButtonText } from '@gluestack-ui/themed';
+import { Button } from '@gluestack-ui/themed';
 import { VStack } from '@gluestack-ui/themed';
 
 import React from 'react';
@@ -9,11 +9,11 @@ type MyButtonStory = ComponentStory<typeof Button>;
 const ButtonStylesExample: MyButtonStory = ({}) => {
   const variants = ['solid', 'outline', 'link'];
   return (
-    <VStack space="md">
+    <VStack>
       {variants.map((variant: any) => {
         return (
           <Button variant={variant} mt="$4">
-            <ButtonText>{variant}</ButtonText>
+            {variant}
           </Button>
         );
       })}
