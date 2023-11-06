4ced216d9a1b8ad17d22341b24959cc49eafc9a7
Vidhi Kataria <katariavidhi99@gmail.com>
2023-11-06 15:24:07 +0530

    fix: button

--- example/storybook/src/components/Forms/Button/ButtonSizes.tsx
+++ example/storybook/src/components/Forms/Button/ButtonSizes.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import type { ComponentStory } from '@storybook/react-native';
-import {
-  Button,
-  ButtonText,
-  ButtonIcon,
-  VStack,
-  AddIcon,
-} from '@gluestack-ui/themed';
+import { Button, VStack } from '@gluestack-ui/themed';
 
 type MyButtonStory = ComponentStory<typeof Button>;
 
 const ButtonSizesExample: MyButtonStory = ({}) => {
   const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
   return (
-    <VStack space="md" justifyContent="center" alignItems="center">
+    <VStack justifyContent="center" alignItems="center">
       {sizes.map((size: any) => {
         return (
           <Button mt="$4" size={size} key={size}>
-            <ButtonText>Button</ButtonText>
-            <ButtonIcon as={AddIcon} ml="$2" />
+            Button
+            {/* <Button.Icon as={AddIcon} ml="$2" /> */}
           </Button>
         );
       })}
