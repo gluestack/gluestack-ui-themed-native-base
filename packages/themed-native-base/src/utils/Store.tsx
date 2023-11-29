31818482e268d978df88655561e036b86794784b
Akash Gautam <akashgautam@geekyants.com>
2023-11-29 15:54:59 +0530

    feat: store class init

---
+++ packages/themed-native-base/src/utils/Store.tsx
@@ -0,0 +1,23 @@
+export class VariantStore {
+  store: any = {};
+  createVariant: any;
+  // getVariant: any;
+
+  constructor(init: any) {
+    this.store = init ?? {};
+    // this.getVariant = (Component) => {
+    //   this.store.Component;
+    // };
+  }
+
+  getVariants(componentName: string) {
+    if (this.store[`${componentName}`]) {
+      return this.store[`${componentName}`];
+    }
+    return {};
+  }
+
+  addVariant(componentName: string, variantName: string, variantCallback: any) {
+    this.store[`${componentName}`][`${variantName}`] = variantCallback;
+  }
+}
