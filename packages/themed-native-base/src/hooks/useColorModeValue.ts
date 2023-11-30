5965cbc1065ed4851daddef0dfef42ba5b487670
Akash Gautam <akashgautam@geekyants.com>
2023-11-30 15:18:57 +0530

    fix: useCMV hook

--- packages/themed-native-base/src/hooks/useColorModeValue.ts
+++ packages/themed-native-base/src/hooks/useColorModeValue.ts
@@ -1,9 +1,8 @@
 import { useContext } from 'react';
 import { HooksContext } from '../components/Provider';
-import { useColorMode } from './useColorMode';
 
 export function useColorModeValue(prop1: any, prop2: any) {
-  const { colorMode } = useColorMode();
+  const { colorMode } = useContext(HooksContext);
   if (colorMode === 'light') return prop1;
   return prop2;
 }
