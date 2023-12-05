db3be2eb2e190495b630710e64de2cd9e7dc7019
Akash Gautam <akashgautam@geekyants.com>
2023-12-05 16:03:16 +0530

    fix: formcontrol error icon

--- packages/themed-native-base/src/components/FormControl/styled-components/Error.tsx
+++ packages/themed-native-base/src/components/FormControl/styled-components/Error.tsx
@@ -9,8 +9,19 @@ export default styled(
     alignItems: 'center',
     mt: '$1',
     gap: '$1',
+    _icon: {
+      textAlign: 'center',
+      justifyContent: 'center',
+      color: '$error.600',
+      _dark: {
+        color: '$error.500',
+      },
+      height: '$4',
+      width: '$4',
+    },
   },
   {
     componentName: 'FormControlError',
+    descendantStyle: ['_icon'],
   } as const
 );
