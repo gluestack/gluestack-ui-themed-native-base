24de93223e65b9d45c80171a7ca58491e13fcae2
Akash Gautam <akashgautam@geekyants.com>
2023-12-20 01:42:16 +0530

    fix: collapse

--- packages/themed-native-base/src/components/Collapse/index.tsx
+++ packages/themed-native-base/src/components/Collapse/index.tsx
@@ -13,78 +13,78 @@ function usePrevious(value: any) {
   return { value: ref.current, updatePrevious };
 }
 
-const Collapse = (
-  {
-    endingHeight,
-    startingHeight,
-    duration,
-    // animateOpacity,
-    isOpen,
-    onAnimationEnd,
-    onAnimationStart,
-    ...props
-  }: any,
-  ref?: any
-) => {
-  if (Platform.OS === 'android') {
-    UIManager.setLayoutAnimationEnabledExperimental &&
-      UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
-  const CustomLayoutLinear = {
-    duration: duration ? duration : 400,
-    create: {
-      type: LayoutAnimation.Types.easeInEaseOut,
-      property: LayoutAnimation.Properties.opacity,
-    },
-    update: {
-      type: LayoutAnimation.Types.easeInEaseOut,
-    },
-  };
-  const defaultStartHeight: any = isOpen
-    ? endingHeight
-    : startingHeight
-    ? startingHeight
-    : 1;
-  let animatedStyle = { height: defaultStartHeight };
-  const animateView = () => {
-    if (onAnimationStart) {
-      onAnimationStart();
+export const Collapse = forwardRef(
+  (
+    {
+      endingHeight,
+      startingHeight,
+      duration,
+      // animateOpacity,
+      isOpen,
+      onAnimationEnd,
+      onAnimationStart,
+      ...props
+    }: any,
+    ref?: any
+  ) => {
+    if (Platform.OS === 'android') {
+      UIManager.setLayoutAnimationEnabledExperimental &&
+        UIManager.setLayoutAnimationEnabledExperimental(true);
     }
-    animatedStyle = {
-      height: isOpen ? endingHeight : defaultStartHeight,
+    const CustomLayoutLinear = {
+      duration: duration ? duration : 400,
+      create: {
+        type: LayoutAnimation.Types.easeInEaseOut,
+        property: LayoutAnimation.Properties.opacity,
+      },
+      update: {
+        type: LayoutAnimation.Types.easeInEaseOut,
+      },
     };
-    const callback = onAnimationEnd ? onAnimationEnd : () => {};
-    LayoutAnimation.configureNext(CustomLayoutLinear, callback());
-  };
+    const defaultStartHeight: any = isOpen
+      ? endingHeight
+      : startingHeight
+      ? startingHeight
+      : 1;
+    let animatedStyle = { height: defaultStartHeight };
+    const animateView = () => {
+      if (onAnimationStart) {
+        onAnimationStart();
+      }
+      animatedStyle = {
+        height: isOpen ? endingHeight : defaultStartHeight,
+      };
+      const callback = onAnimationEnd ? onAnimationEnd : () => {};
+      LayoutAnimation.configureNext(CustomLayoutLinear, callback());
+    };
 
-  const wasOpen = usePrevious(isOpen);
-  if (wasOpen?.value && wasOpen.value !== isOpen) {
-    animateView();
-    wasOpen.updatePrevious(isOpen);
-  }
-  const [size, setSize] = React.useState(startingHeight ?? 0);
-  const provideSize = (layoutSize: any) => {
-    setSize(layoutSize.height);
-  };
-  const _web = {
-    transition: `height ${duration ?? '400'}ms`,
-    height: isOpen ? endingHeight || size : startingHeight || 0,
-  };
+    const wasOpen = usePrevious(isOpen);
+    if (wasOpen?.value && wasOpen.value !== isOpen) {
+      animateView();
+      wasOpen.updatePrevious(isOpen);
+    }
+    const [size, setSize] = React.useState(startingHeight ?? 0);
+    const provideSize = (layoutSize: any) => {
+      setSize(layoutSize.height);
+    };
+    const _web = {
+      transition: `height ${duration ?? '400'}ms`,
+      height: isOpen ? endingHeight || size : startingHeight || 0,
+    };
 
-  return (
-    <Box
-      style={{ ...animatedStyle, ...(Platform.OS === 'web' && _web) }}
-      overflow="hidden"
-      ref={ref}
-    >
+    return (
       <Box
-        //@ts-ignore
-        overflow={Platform.OS === 'web' ? 'auto' : 'scroll'}
-        onLayout={(e: any) => provideSize(e.nativeEvent.layout)}
-        {...props}
-      />
-    </Box>
-  );
-};
-
-export default React.memo(forwardRef(Collapse));
+        style={{ ...animatedStyle, ...(Platform.OS === 'web' && _web) }}
+        overflow="hidden"
+        ref={ref}
+      >
+        <Box
+          //@ts-ignore
+          overflow={Platform.OS === 'web' ? 'auto' : 'scroll'}
+          onLayout={(e: any) => provideSize(e.nativeEvent.layout)}
+          {...props}
+        />
+      </Box>
+    );
+  }
+);
