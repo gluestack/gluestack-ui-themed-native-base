53295056b206573e95d3a5e9e9a86bc9e3a98c0c
Akash Gautam <akashgautam@geekyants.com>
2023-12-20 13:43:45 +0530

    fix: gradient in all layout components

--- packages/themed-native-base/src/components/AspectRatio/index.tsx
+++ packages/themed-native-base/src/components/AspectRatio/index.tsx
@@ -12,7 +12,8 @@ type IRatio = {
 
 import { usePropResolution } from '../../hooks/usePropResolution';
 import { GenericComponentType } from '../../types';
-import { useBreakpointValue } from '../../hooks';
+import { useBreakpointValue, useColorModeValue } from '../../hooks';
+import { HooksContext } from '../Provider';
 
 const AspectRatioTemp = forwardRef(
   ({ children, ratio, ...props }: any, ref?: any) => {
@@ -23,6 +24,71 @@ const AspectRatioTemp = forwardRef(
       if (typeof child === 'string') return <Text>{child}</Text>;
       return child;
     });
+
+    const { config }: any = React.useContext<any>(HooksContext);
+    const Gradient = config?.['linear-gradient'];
+
+    const lightModeGradient =
+      props?._light?.bg?.linearGradient ||
+      props?._light?.background?.linearGradient ||
+      props?._light?.bgColor?.linearGradient ||
+      props?._light?.backgroundColor?.linearGradient;
+
+    const darkModeGradient =
+      props?._dark?.bg?.linearGradient ||
+      props?._dark?.background?.linearGradient ||
+      props?._dark?.bgColor?.linearGradient ||
+      props?._dark?.backgroundColor?.linearGradient;
+
+    const neutralGradient =
+      props?.bg?.linearGradient ||
+      props?.background?.linearGradient ||
+      props?.bgColor?.linearGradient ||
+      props?.backgroundColor?.linearGradient;
+
+    const colorModeGradient = useColorModeValue(
+      lightModeGradient || neutralGradient,
+      darkModeGradient || neutralGradient
+    );
+
+    if (colorModeGradient) {
+      const lgrad = colorModeGradient;
+
+      delete resolvedPropForGluestack.sx['@linearGradient'];
+
+      if (Gradient) {
+        let startObj = { x: 0, y: 0 };
+        let endObj = { x: 0, y: 1 };
+        if (lgrad.start && lgrad.start.length === 2) {
+          startObj = {
+            x: lgrad.start[0],
+            y: lgrad.start[1],
+          };
+        }
+        if (lgrad.end && lgrad.end.length === 2) {
+          endObj = {
+            x: lgrad.end[0],
+            y: lgrad.end[1],
+          };
+        }
+
+        return (
+          <AccessibleAspectRatio
+            as={Gradient}
+            aspectRatio={aspectRatio}
+            {...resolvedPropForGluestack}
+            colors={lgrad.colors}
+            start={startObj}
+            end={endObj}
+            locations={lgrad.locations}
+            ref={ref}
+          >
+            {GUIChildren}
+          </AccessibleAspectRatio>
+        );
+      }
+    }
+
     return (
       <AccessibleAspectRatio
         aspectRatio={aspectRatio}
--- packages/themed-native-base/src/components/Stack/index.tsx
+++ packages/themed-native-base/src/components/Stack/index.tsx
@@ -1,8 +1,11 @@
+// @ts-nocheck
 import React, { Children, cloneElement, forwardRef } from 'react';
 import { Root as AccessibleStack } from './styled-components';
 import { usePropResolution } from '../../hooks/usePropResolution';
 import { GenericComponentType } from '../../types';
 import { Text } from '../Text';
+import { HooksContext } from '../Provider';
+import { useColorModeValue } from '../../hooks';
 
 type IProps = Omit<React.ComponentProps<typeof AccessibleStack>, 'direction'>;
 
@@ -17,7 +20,6 @@ const StackTemp = forwardRef(
     { children, divider, direction, ...props }: IProps & StackProps,
     ref?: any
   ) => {
-    // @ts-ignore
     props.flexDirection = props.flexDirection ?? props.flexDir ?? direction;
     const resolvedPropForGluestack = usePropResolution(props);
     const Divider =
@@ -28,6 +30,79 @@ const StackTemp = forwardRef(
             ? 'vertical'
             : 'horizontal',
       });
+
+    const { config }: any = React.useContext<any>(HooksContext);
+    const Gradient = config?.['linear-gradient'];
+
+    const lightModeGradient =
+      props?._light?.bg?.linearGradient ||
+      props?._light?.background?.linearGradient ||
+      props?._light?.bgColor?.linearGradient ||
+      props?._light?.backgroundColor?.linearGradient;
+
+    const darkModeGradient =
+      props?._dark?.bg?.linearGradient ||
+      props?._dark?.background?.linearGradient ||
+      props?._dark?.bgColor?.linearGradient ||
+      props?._dark?.backgroundColor?.linearGradient;
+
+    const neutralGradient =
+      props?.bg?.linearGradient ||
+      props?.background?.linearGradient ||
+      props?.bgColor?.linearGradient ||
+      props?.backgroundColor?.linearGradient;
+
+    const colorModeGradient = useColorModeValue(
+      lightModeGradient || neutralGradient,
+      darkModeGradient || neutralGradient
+    );
+
+    if (colorModeGradient) {
+      const lgrad = colorModeGradient;
+
+      delete resolvedPropForGluestack.sx['@linearGradient'];
+
+      if (Gradient) {
+        let startObj = { x: 0, y: 0 };
+        let endObj = { x: 0, y: 1 };
+        if (lgrad.start && lgrad.start.length === 2) {
+          startObj = {
+            x: lgrad.start[0],
+            y: lgrad.start[1],
+          };
+        }
+        if (lgrad.end && lgrad.end.length === 2) {
+          endObj = {
+            x: lgrad.end[0],
+            y: lgrad.end[1],
+          };
+        }
+
+        return (
+          <AccessibleStack
+            as={Gradient}
+            {...resolvedPropForGluestack}
+            colors={lgrad.colors}
+            start={startObj}
+            end={endObj}
+            locations={lgrad.locations}
+            ref={ref}
+          >
+            {Children.map(children, (child, index) => {
+              if (index !== 0)
+                return (
+                  <>
+                    {Divider && Divider}
+                    {typeof child === 'string' ? <Text>{child}</Text> : child}
+                  </>
+                );
+              return child;
+            })}
+          </AccessibleStack>
+        );
+      }
+    }
+
     return (
       <AccessibleStack
         gap={resolvedPropForGluestack.space}
