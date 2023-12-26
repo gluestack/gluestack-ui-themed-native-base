d44f8348ad3c1ac27c5519f08bcc93f215a907c7
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-12-26 11:49:33 +0530

    fix: storybook fixes

--- example/storybook/.ondevice/preview.js
+++ example/storybook/.ondevice/preview.js
@@ -1,33 +1,22 @@
 import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
 import { addParameters } from '@storybook/client-api';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
+import {
+  Center,
+  GluestackUIProvider,
+  NativeBaseProvider,
+  extendTheme,
+} from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/themed';
 import { useState } from 'react';
 
 // export const decorators = [];
 //
-export const parameters = {
-  backgrounds: {
-    default: 'plain',
-    values: [
-      { name: 'plain', value: 'white' },
-      { name: 'warm', value: 'hotpink' },
-      { name: 'cool', value: 'deepskyblue' },
-    ],
-  },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-};
-
 import { useDarkMode } from '../src/hooks/useDarkMode';
 import { Platform } from 'react-native';
 
+export const parameters = {};
+
 export const decorators = [
-  withBackgrounds,
   (Story) => {
     let value = false;
 
@@ -45,40 +34,76 @@ export const decorators = [
         return isDark ? 'dark' : 'light';
       }
     }
+
+    // console.log(NativeBaseProvider, "h")
+
     return (
-      <GluestackUIProvider config={config.theme}>
-        <Story />
-      </GluestackUIProvider>
+      <NativeBaseProvider
+        config={{
+          dependencies: {
+            'linear-gradient': require('expo-linear-gradient').LinearGradient,
+          },
+        }}
+        theme={extendTheme({
+          colors: {
+            // Add new color
+            primary: {
+              50: '#E3F2F9',
+              100: '#C5E4F3',
+              200: '#A2D4EC',
+              300: '#7AC1E4',
+              400: '#47A9DA',
+              500: '#0088CC',
+              600: '#007AB8',
+              700: '#006BA1',
+              800: '#005885',
+              900: '#003F5E',
+            },
+            // Redefining only one shade, rest of the color will remain same.
+            amber: {
+              400: '#d97706',
+            },
+          },
+
+          components: {
+            Box: {
+              baseStyle: {
+                color: 'emerald.400',
+              },
+              defaultProps: {
+                size: 'lg',
+              },
+              sizes: {
+                xl: {
+                  fontSize: '64px',
+                },
+                lg: {
+                  fontSize: '32px',
+                },
+                md: {
+                  fontSize: '16px',
+                },
+                sm: {
+                  fontSize: '12px',
+                },
+              },
+            },
+          },
+        })}
+        colorMode={getColorMode()}
+      >
+        <Center>
+          <Story />
+        </Center>
+      </NativeBaseProvider>
     );
+
+    // return (
+    //   <GluestackUIProvider config={config.theme} colorMode={getColorMode()}>
+    //     <Center>
+    //       <Story />
+    //     </Center>
+    //   </GluestackUIProvider>
+    // );
   },
 ];
-
-addParameters({
-  docs: {
-    container: ({ children, context }) => {
-      let value = false;
-
-      if (Platform.OS === 'web') {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        value = useDarkMode();
-      }
-      const [isDark] = useState(false);
-
-      function getColorMode() {
-        //@ts-ignore
-        if (Platform.OS === 'web') {
-          return value ? 'dark' : 'light';
-        } else {
-          return isDark ? 'dark' : 'light';
-        }
-      }
-      return (
-        <DocsContainer context={context}>
-          <GluestackUIProvider config={config.theme} colorMode={getColorMode()}>
-            {children}
-          </GluestackUIProvider>
-        </DocsContainer>
-      );
-    },
-  },
-});
