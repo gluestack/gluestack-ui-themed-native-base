2cfaae77838e156a1921ab317239ffe4b393066a
Akash Gautam <akashgautam@geekyants.com>
2024-01-11 15:36:22 +0530

    fix: input props

--- packages/themed-native-base/src/components/Input/index.tsx
+++ packages/themed-native-base/src/components/Input/index.tsx
@@ -28,14 +28,27 @@ type InputProps = {
 
 const InputTemp = forwardRef(
   (
-    { InputLeftElement, InputRightElement, placeholder, type, ...props }: any,
+    {
+      InputLeftElement,
+      InputRightElement,
+      placeholder,
+      type,
+      onChangeText,
+      value,
+      ...props
+    }: any,
     ref?: any
   ) => {
     const resolvedProps = usePropResolution(props);
     return (
       <AccessibleInput ref={ref} {...resolvedProps}>
         {InputLeftElement && InputLeftElement}
-        <AccessibleInput.Input placeholder={placeholder} type={type} />
+        <AccessibleInput.Input
+          placeholder={placeholder}
+          value={value}
+          type={type}
+          onChangeText={onChangeText}
+        />
         {InputRightElement && InputRightElement}
       </AccessibleInput>
     );
